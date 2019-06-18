4e97c97811b035fa43bf3b65f53ea2b23ce00a62
aofleejay <aofleejay@hotmail.com>
2019-06-18 20:01:56 +0700

    :sparkles: Try some string, number, array and object.
    Try some string, number, array and object.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-var hello = 'world';
+var hello = 'world'
+var age = 20
+var fruits = ['apple', 'orange', 'pineapple']
+var student = {
+  name: 'John',
+  age: 13,
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
 const hello: string = 'world'
+
+const age: number = 20
+
+const fruits: string[] = ['apple', 'orange', 'pineapple']
+
+const student: { name: string; age: number } = {
+  name: 'John',
+  age: 13,
+}
