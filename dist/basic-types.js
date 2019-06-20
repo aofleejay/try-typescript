018805a4255a388eb4e5c0501333bf59a17594ed
คุณาพจน์ ไพรัตน์ <aofleejay@hotmail.com>
2019-06-20 23:31:32 +0700

    :sparkles: Add an object.
    Add an object.

--- dist/basic-types.js
+++ dist/basic-types.js
@@ -1,4 +1,23 @@
+// String
 var fullname = 'John Doe';
+var greeting = "Hello " + fullname;
+var paragraph = "lorem ipsum\n  dolor sit amet\n";
+// Number
 var age = 26;
+var price = 9.99;
+var temperature = -10;
+// Boolean
 var isMarried = false;
+// Array
 var skills = ['JavaScript', 'TypeScript'];
+// Tuple
+var product = [1, 'Pencil', 15.0];
+// Object
+var student = {
+    id: 1,
+    firstname: 'John',
+    lastname: 'Doe',
+    getFullname: function () {
+        return this.firstname + " " + this.lastname;
+    }
+};
--- src/basic-types.ts
+++ src/basic-types.ts
@@ -1,4 +1,35 @@
+// String
 const fullname: string = 'John Doe'
+const greeting: string = `Hello ${fullname}`
+const paragraph: string = `lorem ipsum
+  dolor sit amet
+`
+
+// Number
 const age: number = 26
+const price: number = 9.99
+const temperature: number = -10
+
+// Boolean
 const isMarried: boolean = false
+
+// Array
 const skills: string[] = ['JavaScript', 'TypeScript']
+
+// Tuple
+const product: [number, string, number] = [1, 'Pencil', 15.0]
+
+// Object
+const student: {
+  id: number
+  firstname: string
+  lastname: string
+  getFullname(): string
+} = {
+  id: 1,
+  firstname: 'John',
+  lastname: 'Doe',
+  getFullname() {
+    return `${this.firstname} ${this.lastname}`
+  },
+}
